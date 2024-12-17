52d866ac77a8f30ee05bf9250479841d5b778c7a
yunaArai <araiyunaaa59@gmail.com>
2024-12-18 00:07:31 +0900

    Initial commit

---
+++ assets/script/slidar.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    focusOnSelect: true
+                }
+            }
+        ]
+    });
+});
